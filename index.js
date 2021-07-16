b69cb855c757f1b677e06e71b81c8fb9954edf0e
Geoffery Blackard <64033717+gblackard-gvtx@users.noreply.github.com>
2021-07-16 13:27:19 -0500

    First Commit
    First Commit of the TNFTB

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const tmi = require('tmi.js');
+
+const client = new tmi.Client({
+	options: { debug: true },
+	identity: {
+		username: process.env.BOT_USERNAME ,
+		password: process.env.OAUTH_TOKEN
+	},
+	channels: [ 'airaden' ]
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+	// Ignore echoed messages.
+	if(self) return;
+
+	if(message.toLowerCase() === '!hello') {
+		// "@alca, heya!"
+		client.say(channel, `@${tags.username}, heya!`);
+	}
+
+    if(message.toLocaleLowerCase() === '!dice'){
+        const num = rollDice();
+        client.say(channel, `@${tags.username}, You rolled a ${num}`);
+    }
+});
+
+const rollDice =()=>{
+  const sides = 6;
+  return Math.floor(Math.random() * sides) + 1;
+}
