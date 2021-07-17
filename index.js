ad27e98c5fe59f6eae3fd769ffcca99659631495
Geoffery Blackard <64033717+gblackard-gvtx@users.noreply.github.com>
2021-07-17 14:52:30 -0500

    Update index.js
    Working on the connection via application to use Clip capability

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const client = new tmi.Client({
 		username: process.env.BOT_USERNAME ,
 		password: process.env.OAUTH_TOKEN
 	},
-	channels: [ 'airaden' ]
+	channels: [ 'nonfungbot' ]
 });
 
 client.connect();
@@ -24,10 +24,20 @@ client.on('message', (channel, tags, message, self) => {
     if(message.toLocaleLowerCase() === '!dice'){
         const num = rollDice();
         client.say(channel, `@${tags.username}, You rolled a ${num}`);
+
     }
+	if (message.toLocaleLowerCase()=== '!clip') {
+		const userAction = async () => {
+			const response = await fetch('https://usr9herp2j.execute-api.us-east-2.amazonaws.com/default/NFTClipTwitch%27');
+			const myJson = await response.json(); //extract JSON from the http response
+		   console.log(myJson);
+		  }
+	
+	}
 });
 
 const rollDice =()=>{
   const sides = 6;
   return Math.floor(Math.random() * sides) + 1;
 }
+
