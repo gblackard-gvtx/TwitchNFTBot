abb059c6a994661b59883834549e11a9c7b9c74f
Thomas Braccia <bracciata@gmail.com>
2021-07-17 15:59:13 -0500

    Began refractoring Library so AWS is independent

--- index.js
+++ index.js
@@ -4,30 +4,30 @@ const tmi = require('tmi.js');
 const client = new tmi.Client({
 	options: { debug: true },
 	identity: {
-		username: process.env.BOT_USERNAME ,
+		username: process.env.BOT_USERNAME,
 		password: process.env.OAUTH_TOKEN
 	},
-	channels: [ 'airaden' ]
+	channels: ['airaden']
 });
 
 client.connect();
 
 client.on('message', (channel, tags, message, self) => {
 	// Ignore echoed messages.
-	if(self) return;
+	if (self) return;
 
-	if(message.toLowerCase() === '!hello') {
+	if (message.toLowerCase() === '!hello') {
 		// "@alca, heya!"
 		client.say(channel, `@${tags.username}, heya!`);
 	}
 
-    if(message.toLocaleLowerCase() === '!dice'){
-        const num = rollDice();
-        client.say(channel, `@${tags.username}, You rolled a ${num}`);
-    }
+	if (message.toLocaleLowerCase() === '!dice') {
+		const num = rollDice();
+		client.say(channel, `@${tags.username}, You rolled a ${num}`);
+	}
 });
 
-const rollDice =()=>{
-  const sides = 6;
-  return Math.floor(Math.random() * sides) + 1;
+const rollDice = () => {
+	const sides = 6;
+	return Math.floor(Math.random() * sides) + 1;
 }
