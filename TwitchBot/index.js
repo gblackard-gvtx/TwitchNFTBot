87c8803e997fae5b4ce87114e275afef2dcbd1bc
Geoffery Blackard <64033717+gblackard-gvtx@users.noreply.github.com>
2021-07-18 11:24:34 -0500

    Update index.js
    working on authintication and refresh access code logic.

--- TwitchBot/index.js
+++ TwitchBot/index.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
-const tmi = require('tmi.js');
-const fetch = require("node-fetch");
+const tmi = require('tmi.js');;
+const fetch = require('node-fetch');
 
-const client = new tmi.Client({
+/*const client = new tmi.Client({
 	options: { debug: true },
 	identity: {
 		username: process.env.BOT_USERNAME,
@@ -12,34 +12,27 @@ const client = new tmi.Client({
 });
 
 client.connect();
-
-client.on('message', (channel, tags, message, self) => {
-	// Ignore echoed messages.
-	if (self) return;
-
-	if (message.toLowerCase() === '!hello') {
-		// "@alca, heya!"
-		client.say(channel, `@${tags.username}, heya!`);
-	}
-
-	if (message.toLocaleLowerCase() === '!dice') {
-		const num = rollDice();
-		client.say(channel, `@${tags.username}, You rolled a ${num}`);
-
-	}
-	if (message.toLocaleLowerCase() === '!clip') {
-		const AWSEndpointCall = async () => {
-			const response = await fetch('https://usr9herp2j.execute-api.us-east-2.amazonaws.com/default/NFTClipTwitch');
-			const myJson = await response.json(); //extract JSON from the http response}
-			client.say(channel, `@${tags.username}, ${JSON.stringify(myJson)}`);
-		}
-		AWSEndpointCall();
-
-	}
-});
-
-const rollDice = () => {
-	const sides = 6;
-	return Math.floor(Math.random() * sides) + 1;
+*/
+async function getRefreshedAccessToken(){
+	const refreshToken = process.env.REFRESH;
+	const clientId = process.env.CLIENT;
+	const secret = process.env.SECRET;
+	const response = await fetch(`https://id.twitch.tv/oauth2/token?grant_type=refresh_token&refresh_token=${refreshToken}&client_id=${clientId}&client_secret=${secret}`,{
+		method: 'post'
+	});
+	const data = await response.json();
+console.log(data);
 }
 
+const main = async()=>{
+	let accessToken;
+	let responseClipURL;
+	let clipId;
+	try {
+    getRefreshedAccessToken();
+    } catch( error ) {
+        console.error("problem-fetching-access-token", error);
+        return "Unexpected problem when fetching the access token.";
+    }
+}
+ main();
