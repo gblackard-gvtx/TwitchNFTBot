d4d1b17cff68bb2d4aab3316f6ba965dd71ac5fe
Geoffery Blackard <64033717+gblackard-gvtx@users.noreply.github.com>
2021-07-21 01:43:49 -0500

    Update AWS endpoint and twitchbot
    got the clip function working.

--- TwitchBot/index.js
+++ TwitchBot/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const tmi = require('tmi.js');;
 const fetch = require('node-fetch');
 
-/*const client = new tmi.Client({
+const client = new tmi.Client({
 	options: { debug: true },
 	identity: {
 		username: process.env.BOT_USERNAME,
@@ -12,15 +12,13 @@ const fetch = require('node-fetch');
 });
 
 client.connect();
-*/
+
 
 const APP_CLIENT_ID = process.env.CLIENT;
 const APP_CLIENT_SECRET = process.env.SECRET;
 const APP_REFRESH_TOKEN = process.env.REFRESH;
 const CHANNEL_BROADCAST_ID = process.env.BROADCASTID;
-console.log(APP_CLIENT_ID)
-console.log(APP_CLIENT_SECRET)
-console.log(APP_REFRESH_TOKEN)
+
 
 client.on('message', (channel, tags, message, self) => {
 	if (self) return;
