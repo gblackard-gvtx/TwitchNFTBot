4580d4d547038657c4e5658d42824d63335f9406
Geoffery Blackard <64033717+gblackard-gvtx@users.noreply.github.com>
2021-07-24 23:29:13 -0500

    Update getclipInfo.py
    finished py function to get clip info
    username, title

--- PythonGetTwitchInfo/getclipInfo.py
+++ PythonGetTwitchInfo/getclipInfo.py
@@ -1,12 +1,36 @@
+mport os
 import requests
-from dotenv import dotenv_values
+import os
+import json
+from dotenv import load_dotenv
 
-config = dotenv_values(".env")
+load_dotenv('D:\\Users\\Adam\\TwitchNFTBot\\TwitchNFTBot\\PythonGetTwitchInfo\\.env')
+
 clipID = 'AnimatedAdventurousHumanKappaPride-8Il1C4EdtT92Bq3n'
-url = 'https://api.twitch.tv/kraken/clips/{0}'.format(clipID)
-CLIENT = config.get('CLIENT')
-headers = {'Accept': 'application/vnd.twitchtv.v5+json', 'Client-ID':CLIENT }
+clipURL = 'https://api.twitch.tv/helix/clips'
+authURL = 'https://id.twitch.tv/oauth2/token'
+CLIENT = os.getenv('CLIENT')
+refresh = os.getenv('REFRESH')
+secret= os.getenv('SECRET')
+rATQuery = {'grant_type':'refresh_token', 'refresh_token': refresh, 'client_id':CLIENT, 'client_secret': secret}
+gCQuery = {'id':clipID}
+# uses refresh token to get new access token
+def getRefreshAccessToken():
+    response = requests.post(authURL,params=rATQuery)
+    json = response.json()
+    access_token = json['access_token']
+    return access_token
 
-response = requests.get(url,headers=headers)
 
+# pass in access token to get the username and the title of the clip 
+def getClip(accessToken):
+    headers = {'Authorization': 'Bearer ' + accessToken, 'Client-ID':CLIENT }
+    response = requests.get(clipURL,params=gCQuery,headers=headers)
+    if response.status_code == 200:
+        json = response.json()
+        userName = json['data'][0]['creator_name']
+        title = json['data'][0]['title']
+    return userName,title
 
+access_token = getRefreshAccessToken()
+getClip(access_token)
