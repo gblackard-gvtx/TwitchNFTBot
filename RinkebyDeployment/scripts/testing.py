6fc563c5ed8dc8e729b978b55d05dbb3e24e5859
Thomas Braccia <bracciata@gmail.com>
2021-08-10 13:04:44 -0400

    Corrected mistake in the uploaded tests

--- RinkebyDeployment/scripts/testing.py
+++ RinkebyDeployment/scripts/testing.py
@@ -3,9 +3,9 @@ from advanced_collectible.create_nft_from_twitch_nftstore import pin_nft_to_nfts
 def test_pin_json():
     assert pinMetadata('testing.json') == 'bafkreih542xjbriydqjipek7gbtsmktbnii6bnom732xjvkuumrnp4ezoa', "Should be bafkreih542xjbriydqjipek7gbtsmktbnii6bnom732xjvkuumrnp4ezoa"
 def test_pin_video():
-    assert pin_nft_to_nftstore('testing.mp4') == 'bafybeifz5gw6h2aoik3fa745rjzbi5hgtsjqmgys2a6mmskdw5zgtxtt5i', "Should be bafybeifz5gw6h2aoik3fa745rjzbi5hgtsjqmgys2a6mmskdw5zgtxtt5i"
+    assert pin_nft_to_nftstore('testing.mp4') == 'bafybeide3g67aazuwlijqdwreqeddioyn5wfj4b3uidh6zxtpemdaimmue', "Should be bafybeide3g67aazuwlijqdwreqeddioyn5wfj4b3uidh6zxtpemdaimmue"
 def test_pin_video_small():
-    assert pin_nft_to_nftstore('testing-small.mp4') == 'bafybeide3g67aazuwlijqdwreqeddioyn5wfj4b3uidh6zxtpemdaimmue', "Should be bafybeide3g67aazuwlijqdwreqeddioyn5wfj4b3uidh6zxtpemdaimmue"
+    assert pin_nft_to_nftstore('testing-small.mp4') == 'bafybeiew466bk3caift2gsnzeb23qmzmpqnim32utahanj5f5ks2ycvk7y', "Should be bafybeiew466bk3caift2gsnzeb23qmzmpqnim32utahanj5f5ks2ycvk7y"
 
 if __name__ == "__main__":
     test_pin_json()
