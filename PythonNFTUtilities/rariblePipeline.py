c37c563d464643cf80b0c96b6c319294be91b4bc
Thomas Braccia <bracciata@gmail.com>
2021-08-23 12:02:26 -0500

    Tested the script and added docs for testing.

--- PythonNFTUtilities/rariblePipeline.py
+++ PythonNFTUtilities/rariblePipeline.py
@@ -9,12 +9,14 @@ from scripts.advanced_collectible.create_nft_from_twitch import pin_file_to_ipfs
 from scripts.advanced_collectible.create_clip import create_clip
 import cv2 as cv
 
+
 def create_clip_and_thumbnail():
     clip_id = create_clip()
     print(f'Clip Id: {clip_id}')
     if clip_id.startswith('Error: '):
         return clip_id
     mint_and_upload_clip(clip_id)
+
 
 def create_thumbnail_and_get_ipfs(path_to_video):
     image_file_name = 'thumbnail.jpeg'
@@ -30,27 +32,28 @@ def create_thumbnail_and_get_ipfs(path_to_video):
     vidcap.release()
     return pin_file_to_ipfs(image_file_name)
 
+
 def mint_and_upload_clip(slug):
     time.sleep(10)
-#    userName = 'testName'
-#    title= 'testTitle'
-    userName, title = get_clip(slug)
+    userName = 'testName'
+    title = 'testTitle'
+    userName, title = get_clip(slug)  # Comment out this line when testing
     print('Username is: ' + userName)
     print('Title is: ' + title)
     print(slug)
-    download_twitch_clip(slug)
+    download_twitch_clip(slug)  # Comment out this line when testing
     # download the video locally using youtube dl and then pass that path below
     path_to_downloaded_video = 'clip.mp4'
     video_ipfs_hash = pin_file_to_ipfs(path_to_downloaded_video)
     print("Ipfs Hash of the Video is: "+video_ipfs_hash)
-    thumbnail_ipfs_hash = create_thumbnail_and_get_ipfs(path_to_downloaded_video)
+    thumbnail_ipfs_hash = create_thumbnail_and_get_ipfs(
+        path_to_downloaded_video)
     # The following is used because youtube_dl doesn't overwrite files with the same name
     print("Ipfs Hash of the thumbnail is: "+thumbnail_ipfs_hash)
 
-    os.remove('clip.mp4')
-    os.remove('thumbnail.jpeg')
-    ipfs_of_metadata = upload_raible_metadata(userName, title, video_ipfs_hash,thumbnail_ipfs_hash)
+    os.remove('clip.mp4')  # Comment out this line when testing
+    os.remove('thumbnail.jpeg')  # Comment out this line when testing
+    ipfs_of_metadata = upload_raible_metadata(
+        userName, title, video_ipfs_hash, thumbnail_ipfs_hash)
     print(ipfs_of_metadata)
     return ipfs_of_metadata
-
-
