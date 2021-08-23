6b65acaa26f64ba1f5dd4b9a77dda782f322bf8d
Thomas Braccia <bracciata@gmail.com>
2021-08-23 12:11:44 -0500

    Cleaned metadata uploading

--- PythonNFTUtilities/rarible/uploadRaribleMetadata.py
+++ PythonNFTUtilities/rarible/uploadRaribleMetadata.py
@@ -1,30 +1,28 @@
 import os
 import requests
 import json
-from metadata import sample_metadata
 from pathlib import Path
 from dotenv import load_dotenv
 import datetime
 
 load_dotenv()
 
-def upload_raible_metadata(streamer,stream_title,videoURL,imageURL):
+
+def upload_raible_metadata(streamer, stream_title, videoURL, imageURL):
     print(imageURL)
     print(stream_title)
-    collectible_metadata={}
-    collectible_metadata["name"] = (f"{streamer}'s Clip created on " + \
-        datetime.datetime.today().strftime("%d/%m/%Y %H:%M %p %Z")).strip()
+    collectible_metadata = {}
+    collectible_metadata["name"] = (f"{streamer}'s Clip created on " +
+                                    datetime.datetime.today().strftime("%d/%m/%Y %H:%M %p %Z")).strip()
     collectible_metadata["description"] = (f"A clip created by {streamer} on " + datetime.datetime.today(
     ).strftime("%d/%m/%Y")+f" during the stream '{stream_title}'").strip()
     collectible_metadata["animation_url"] = "https://ipfs.io/ipfs/" + \
         videoURL+"?filename=video.mp4"
     collectible_metadata["image"] = "https://ipfs.io/ipfs/" + \
-        imageURL+"?filename=image.jpeg"     
+        imageURL+"?filename=image.jpeg"
     collectible_metadata["attributes"] = []
     logs = pinMetadata(collectible_metadata)
     return (logs['IpfsHash'])
-
-# Stolen from https://github.com/Vourhey/pinatapy/blob/master/pinatapy/__init__.py
 
 
 def pinMetadata(json_to_pin, options=None):
